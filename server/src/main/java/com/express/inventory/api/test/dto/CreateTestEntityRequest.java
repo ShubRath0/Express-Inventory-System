7ffd6f5d070cda50a422ab5e2d846145ec6bdd24
Brandon Mancia <bmancia001@gmail.com>
2026-02-18 23:34:46 -0600

    Updated test-api with actual database

---
+++ server/src/main/java/com/express/inventory/api/test/dto/CreateTestEntityRequest.java
@@ -0,0 +1,16 @@
+package com.express.inventory.api.test.dto;
+
+public class CreateTestEntityRequest {
+    private String name;
+
+    public CreateTestEntityRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
