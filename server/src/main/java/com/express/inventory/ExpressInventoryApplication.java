d394c91645504df03a555efa9361fba75403e9aa
Brandon Mancia <bmancia001@gmail.com>
2026-03-24 23:39:58 -0500

    feat: add testing dependencies, implement database connection test, and remove PingService

--- server/src/main/java/com/express/inventory/ExpressInventoryApplication.java
+++ server/src/main/java/com/express/inventory/ExpressInventoryApplication.java
@@ -2,10 +2,8 @@ package com.express.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class ExpressInventoryApplication {
 
     public static void main(String[] args) {
