154407dbb6f19ef8d69e0e195c633f5bbf151c21
teresa_franxman <teresafranxman97@gmail.com>
2026-04-02 10:44:10 -0500

    Created inventory summary report table

--- client/src/features/inventorySummary/InventorySummaryPage.tsx
+++ client/src/features/inventorySummary/InventorySummaryPage.tsx
@@ -1,11 +1,13 @@
 import { Header } from "@/components";
+import { InventoryNav } from "@/features/inventorySummary/components/searchbar";
 import { InventoryTable } from "./components/inventoryTable";
 
 export const InventorySummaryPage = () => {
   return (
     <section className="w-full h-full p-4  flex flex-col gap-4">
-        <Header title="Inventory Summary" />
-        <InventoryTable />
+      <Header title="Inventory Summary" />
+      <InventoryNav />
+      <InventoryTable />
     </section>
   );
 };
