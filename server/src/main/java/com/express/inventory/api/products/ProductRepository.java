6d851e7e35de71943266d8e09290a35040eccdf2
david_jauregui <du6358gk@go.minnstate.edu>
2026-04-13 04:34:37 -0500

    Both Recent added products and all category endpoints seem to work nicely and java application did not crash, we good here, this is the work from the first week, forgot to commit to branch.

--- server/src/main/java/com/express/inventory/api/products/ProductRepository.java
+++ server/src/main/java/com/express/inventory/api/products/ProductRepository.java
@@ -3,10 +3,16 @@ package com.express.inventory.api.products;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
     public List<ProductEntity> findByNameContainingIgnoreCase(String name);
 
     public List<ProductEntity> findByCategory(String category);
+
+    @Query("SELECT DISTINCT products.category FROM ProductEntity products")
+    public List<ProductEntity> findAllCategories();
+
+    public List<ProductEntity> findTop5ByOrderByCreatedAtDesc();
 }
