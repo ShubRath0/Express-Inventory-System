24be9ede324dfb12e06f08b5fd15743824542bed
Teresa Franxman <teresafranxman97@gmail.com>
2026-04-23 00:11:38 -0500

    Updated useGetAllProducts call in InventoryTableContainer.tsx

--- client/src/features/inventorySummary/components/InventoryTableContainer.tsx
+++ client/src/features/inventorySummary/components/InventoryTableContainer.tsx
@@ -1,14 +1,10 @@
 import { useGetAllProducts } from "@/api/__generated__/product-controller/product-controller";
 import { Loading } from "@/components";
-import { useQuery } from "@tanstack/react-query";
 import { InventoryTable } from "./inventoryTable";
 
 export const InventoryTableContainer = () => {
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["products"],
-    queryFn: () => useGetAllProducts({ size: 1000 }),
-  });
-
+  const { data, isLoading, isError } = useGetAllProducts({ size: 1000 });
+  
   if (isLoading) {
     return <Loading label="Loading products..." />;
   }
@@ -17,5 +13,5 @@ export const InventoryTableContainer = () => {
     return <div>Failed to load products</div>;
   }
 
-  return <InventoryTable data={data?.data?.data?.content || []} />;
+  return <InventoryTable data={data?.data?.content || []} />;
 };
